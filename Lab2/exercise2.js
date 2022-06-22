ba3a66fc6f0e8a4648b9205bbbac57a2fb88fade
Chi <chiemi2400@gmail.com>
2022-06-22 02:02:01 -0700

    Add files via upload

---
+++ Lab2/exercise2.js
@@ -0,0 +1,12 @@
+var name = prompt("Enter your name");
+var age = prompt("Enter your age you will be in 2017");
+var birth = 2018 - age
+
+document.getElementById("p1").innerHTML="User's name is " + name +", and birth year is " + birth;
+
+var num1 = prompt("Enter the first number");
+var num2 = prompt("Enter the second number");
+
+var num3 = (num1 + num2) * (num1 + num2) * (num1 + num2)
+
+alert(num3);;;;
